cbf91d43b54e809f253c1e3124e5b076669b5090
Imon <contact@imonweb.co.uk>
2024-11-15 22:53:58 +0000

    activate custom theme support options

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -50,7 +50,7 @@ function sunset_custom_settings() {
 
 //Post Formats Callback Function
 function sunset_post_formats_callback($input){
-  var_dump($input);
+  return $input;
 }
 
  
@@ -60,10 +60,12 @@ function sunset_theme_options() {
 }
 
 function sunset_post_formats() {
+  $options =  get_option( 'post_formats' );
   $formats = array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat');
   $output = '';
   foreach($formats as $format){
-    $output .= '<label><input type="checkbox" id="' . $format . '" name="post_formats[' . $format . ']" value="1"> '. $format .'</label><br>';
+    $checked = (@$options[$format] == 1 ? 'checked' : '');
+    $output .= '<label><input type="checkbox" id="' . $format . '" name="post_formats[' . $format . ']" value="1" '. $checked .'/> '. $format .'</label><br>';
   }
   echo $output;
 }
--- inc/templates/sunset-theme-support.php
+++ inc/templates/sunset-theme-support.php
@@ -1,9 +1,5 @@
 <h1>Sunset Sidebar Options</h1>
 <?php settings_errors(); ?>
-<?php    
-/* ======  ====== */
- // $picture = esc_attr( get_option('profile_picture') );
-?>
 
 <form action="options.php" method="post" class="sunset-general-form">
   <?php settings_fields('sunset-theme-support'); ?>
