ab5a6ea8366e2a285ace8219743596626b7c3a48
Imon <contact@imonweb.co.uk>
2024-11-19 00:29:36 +0000

    create custom contact form

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 require get_template_directory() . '/inc/function-admin.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/theme-support.php';
+require get_template_directory() . '/inc/custom-post-type.php';
 
 /**
  * Proper way to enqueue scripts and styles
