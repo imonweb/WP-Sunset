b5e276bf8f372e4f367ff2b471fbd0b765bef247
Imon <contact@imonweb.co.uk>
2024-11-26 00:01:26 +0000

    standard post format

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c7b6a6065761ea337c9a');
+<?php return array('dependencies' => array(), 'version' => 'b1565bfa0ca3e8bad962');
--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php get_header(); ?>
  
-<pre>
- <?php 
-  $wp_query = new WP_Query( array('category_name' => 'unicorns') );
-
+<?php 
+  // $wp_query = new WP_Query( array('category_name' => 'unicorns') );
   // var_dump($wp_query);
-
- 
-
 ?>
-</pre>
  
+ 
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <div class="container">
+      
+      <?php 
+     
+        if( have_posts() ) :
+          while( have_posts() ) : the_post();
+            get_template_part( 'template-parts/content', get_post_format() );
+          endwhile;
+        endif;
+ 
+      ?>
+    
+    </div>
+  </main>
+</div>
  
 
 <?php get_footer(); ?>
